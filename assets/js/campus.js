d2d200584d74958ba36d22a83a434705c43bad64
jangjiheee <jihee.jang210@gmail.com>
2022-11-21 17:57:49 +0900

    campus 완료

--- assets/js/campus.js
+++ assets/js/campus.js
@@ -1,25 +1,29 @@
 window.onload = function () {
   var depOne = document.querySelectorAll(".li01");
   var depTwo = document.querySelectorAll(".dep2");
-  console.log(depOne);
-  console.log(depTwo);
-
-  // for (let i = 0; i < depOne.length; i++) {
-  //   depOne[i].addEventListener("click", function () {
-  //     this.nextSibling.classList.add("on");
-  //   })
-  // }
-
-  // depOne.addEventListener("click", function () {
-  //   // depTwo.style.height = "350px";
-  //   this.classList.toggle('on');
-  //   depTwo.classList.toggle("on");
-  // })
+  var depThree = document.querySelectorAll(".dep2_li");
+  var threeList = document.querySelectorAll(".dep3");
 
   for (let i = 0; i < depOne.length; i++) {
     depOne[i].addEventListener("click", function () {
-    this.classList.toggle('on');
-    depTwo[i].classList.toggle("on");
-  })
+      this.classList.toggle('on');
+      depTwo[i].classList.toggle("on");
+    })
   }
+
+  for (let i = 0; i < depThree.length; i++) {
+    depThree[i].addEventListener("click", function () {
+      this.classList.toggle('on');
+      threeList[i].classList.toggle("on");
+    })
+  }
+
+  var toggle_btn = document.querySelector(".toggle_btn");
+  var toggle_click = document.querySelector(".array_click");
+  var array = document.querySelector(".array");
+  console.log(toggle_btn);
+  console.log(toggle_click);
+  array.addEventListener("click", function () {
+    toggle_click.classList.toggle("active");
+  })
 }
