7091d083a18d49240be9b9a60ead5df71eb067da
jangjiheee <jihee.jang210@gmail.com>
2022-11-21 15:36:50 +0900

    second commit

--- assets/js/campus.js
+++ assets/js/campus.js
@@ -1,12 +1,25 @@
 window.onload = function () {
-  var depOne = document.querySelectorAll(".dep1");
+  var depOne = document.querySelectorAll(".li01");
   var depTwo = document.querySelectorAll(".dep2");
   console.log(depOne);
   console.log(depTwo);
 
+  // for (let i = 0; i < depOne.length; i++) {
+  //   depOne[i].addEventListener("click", function () {
+  //     this.nextSibling.classList.add("on");
+  //   })
+  // }
+
+  // depOne.addEventListener("click", function () {
+  //   // depTwo.style.height = "350px";
+  //   this.classList.toggle('on');
+  //   depTwo.classList.toggle("on");
+  // })
+
   for (let i = 0; i < depOne.length; i++) {
     depOne[i].addEventListener("click", function () {
-      this.classList.toggle("on");
-    })
+    this.classList.toggle('on');
+    depTwo[i].classList.toggle("on");
+  })
   }
 }
